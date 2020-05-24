77b627b904df4e1f30d51bb5c53611df97aa8681
yeonghyeon <hah_528@naver.com>
2020-05-24 16:06:17 +0900

    ㅎㅎㅎ 고쳤어요

--- puuuub/puuuubdev.dart
+++ puuuub/puuuubdev.dart
@@ -1,9 +1,6 @@
 import 'package:http/http.dart' as http;
-import 'package:html/parser.dart';
 
 main() async {
-  final response = await http.Client().get(Uri.parse('https://example.com/'));
-
-  var document = parse(response.body);
-  print(document.outerHtml);
+  final response = await http.get('https://example.com/');
+  print(response.body);
 }
